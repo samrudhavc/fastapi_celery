afc8e7157d271d6f70b545c3e50d74a7d4ffc0a8
samrudhavc <samrudhavc@gmail.com>
2025-04-28 08:58:35 +0530

    worker updt

--- worker.py
+++ worker.py
@@ -1,9 +1,4 @@
 from celery_config import celery_app
 import celery_app_test
 import celery_app_mail
-
-# from celery_config import celery_app
-# import celery_app_test
-# import celery_app_mail
-
 # This ensures tasks in celery_app_test.py and celery_app_mail.py are registered
